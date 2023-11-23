f72f051ae6c5f99db60cfa1f9dd7657755bbfeb3
imane <imane@imanes-MacBook-Air.local>
2023-11-23 16:18:10 +0100

    basic sun sign retrieval + bug fix

--- main.py
+++ main.py
@@ -3,33 +3,52 @@ from flatlib.geopos import GeoPos
 from flatlib.chart import Chart
 import requests
 
+# Function to get latitude and longitude from location using Nominatim
+def get_lat_long(location):
+    base_url = 'https://nominatim.openstreetmap.org/search'
+    params = {'q': location, 'format': 'json'}
+    response = requests.get(base_url, params=params)
+    data = response.json()
 
-# create birth chart
+    if data:
+        latitude = float(data[0]['lat'])  # Convert latitude to float
+        longitude = float(data[0]['lon'])  # Convert longitude to float
+        return latitude, longitude
+    else:
+        raise ValueError("Could not find the location")
+
+
+# Modified function to create a birth chart
 def create_birth_chart(date, time, location):
-    # Assuming date is in 'YYYY/MM/DD', time in 'HH:MM', location as 'City, Country'
     birth_datetime = Datetime(date, time)
-    birth_location = GeoPos('latitude', 'longitude')  # Get lat/long from location
+    
+    # Retrieve latitude and longitude from location
+    latitude, longitude = get_lat_long(location)
+    birth_location = GeoPos(latitude, longitude)
+
     return Chart(birth_datetime, birth_location)
 
-# simple birthchart analysis/test
+# Simple birthchart analysis/test
 def analyze_chart(chart):
-    # Example: Analyze sun sign
     sun_sign = chart.getObject('Sun').sign
     return f"The sun sign is {sun_sign}."
 
-# main
+# Main
 def main():
-    # user data
-    date = input("Enter your birth date (YYYY/MM/DD): ")
-    time = input("Enter your birth time (HH:MM): ")
-    location = input("Enter your birth location (City, Country): ")
-    
-    # create and analyze user chart
-    chart = create_birth_chart(date, time, location)
-    analysis = analyze_chart(chart)
-    
-    # display result
-    print(analysis)
+    try:
+        # User data
+        date = input("Enter your birth date (YYYY/MM/DD): ")
+        time = input("Enter your birth time (HH:MM): ")
+        location = input("Enter your birth location (City, Country): ")
+        
+        # Create and analyze user chart
+        chart = create_birth_chart(date, time, location)
+        analysis = analyze_chart(chart)
+        
+        # Display result
+        print(analysis)
+    except ValueError as e:
+        print(f"Error: {e}")
 
 if __name__ == "__main__":
     main()
